5ff2e98c408e34593c84c8b4e4d5fa7c8a5e76d7
oohlearn <learnooh@gmail.com>
2022-08-19 10:22:29 +0800

    rewrite answer

--- abGame3.dart
+++ abGame3.dart
@@ -1,31 +1,44 @@
 import 'dart:io';
 import 'dart:math';
 
+// class Answer(){
+//   String a1;
+//   String a2;
+//   String a3;
+//   String a4;
+
+  // Answer(){
+  //   a1 = Random().nextInt(10);
+  //   a2 = Random().nextInt(10);
+  //   a3 = Random().nextInt(10);
+  //   a4 = Random().nextInt(10);
+
+    
+  // }
+// }
+
 class ABGame3{
-  
   start(){
     print("這是ABGame遊戲，請輸入3個完全不重複整數，共有7次機會");
     String result = playGame();
     print(result);
   }
-  String playGame(){
-    var _answer = Random().nextInt(1000);
-    String _answerS = "$_answer";
-    while(_answerS[0] == _answerS[1] || _answerS[0] == _answerS[2] || _answerS[1] == _answerS[2]){
-      // print(_answerS);
-      while(_answerS.length != 3){
-        _answer = Random().nextInt(1000);
-        _answerS = "$_answer";
-        // print(_answerS);
-      }
+
+  String playGame(){ 
+    var _a1 = Random().nextInt(10);
+    var _a2 = Random().nextInt(10);
+    while(_a1 == _a2){
+      _a2 = Random().nextInt(10);
     }
-    
-    String a1 = _answerS[0];
-    String a2 = _answerS[1];
-    String a3 = _answerS[2];
-    print(_answerS);
-    
-    
+    var _a3 = Random().nextInt(10);
+    while(_a3 == _a2 || _a3 == _a1){
+      _a3 = Random().nextInt(10);
+    }
+    String _a1S = "$_a1";
+    String _a2S = "$_a2";
+    String _a3S = "$_a3";
+    print("$_a1S $_a2S $_a3S");
+
     int maxTimes = 7;
     int chance = 7;
     String result = "";
@@ -45,26 +58,26 @@ class ABGame3{
       int a = 0;
       int b = 0;
 
-      if(a1 == g1 && a2 == g2 && a3 == g3){
+      if(_a1S == g1 && _a2S == g2 && _a3S == g3){
         print("Yes");
         break;
       }
-      if(g1 == a1){
+      if(g1 == _a1S){
         a++;
       }
-      if(g2 == a2){
+      if(g2 == _a2S){
         a++;
       }
-      if(g3 == a3){
+      if(g3 == _a3S){
         a++;
       }
-      if(g1 == a2 || g1 == a3){
+      if(g1 == _a2S || g1 == _a3S){
         b++;
       }
-      if(g2 == a1 || g2 == a3){
+      if(g2 == _a1S || g2 == _a3S){
         b++;
       }
-      if(g3 == a2 || g3 == a1){
+      if(g3 == _a2S || g3 == _a1S){
         b++;
       }
       chance--;
